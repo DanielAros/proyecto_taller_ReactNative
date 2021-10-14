05cb54c98b24b353f24226718860e41e9fd85e3b
Ian <ianvelazquez117@gmail.com>
2021-10-14 16:44:08 -0500

    Added index for imports and LogValidation screen

---
+++ Navigation/LogValidation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NavigationContainer } from "@react-navigation/native";
+import MainNav from './MainNav';
+
+const LogValidation = (props) =>{
+
+    return(
+        <NavigationContainer>
+            <MainNav/>
+        </NavigationContainer>
+    );
+}
+export default LogValidation;
---
+++ Screens/index.js
@@ -0,0 +1,7 @@
+export * from './Cart';
+export * from './Details';
+export * from './Home';
+export * from './InitLogin';
+export * from './InitPass';
+export * from './InitUser';
+export * from './Offers';
