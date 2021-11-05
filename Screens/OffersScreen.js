b7744b411f428d5c332dc8ae6aa9ac899719cea8
Daniel Aros Ramírez <danielaros200@gmail.com>
2021-11-05 11:42:42 -0600

    Ejemplo del drawer navigation  añadiendo un input e icono

--- Screens/OffersScreen.js
+++ Screens/OffersScreen.js
@@ -2,7 +2,18 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export const OffersScreen = () => {
-    
+    return(
+        <View style={styles.container}>
+            <Text>Hola Mundo!!</Text>
+        </View>
+    )
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
 // export derfault OffersScreen;
